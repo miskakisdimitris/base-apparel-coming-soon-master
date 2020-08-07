6e307b41251232fd378cff2ad4c115ce73d526a1
mitsos <mitsos@panagiotis-laptop.panagiotis-laptop>
2020-08-07 14:41:34 +0300

    Add existing file

---
+++ build/precache-manifest.17467ce9eefaef30fa35fdc1e433846c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0fed46c703a765e6c19f4a947b07f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08638ceea4e58630adf",
+    "url": "/static/css/main.bacc8a26.chunk.css"
+  },
+  {
+    "revision": "00b5d9da6ec3e5b3c05c",
+    "url": "/static/js/2.1c544a77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c544a77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08638ceea4e58630adf",
+    "url": "/static/js/main.d662d778.chunk.js"
+  },
+  {
+    "revision": "0821cb8d5673201d2f8e",
+    "url": "/static/js/runtime-main.6fc30f98.js"
+  },
+  {
+    "revision": "003b0c9744b45dea37e19234916edb43",
+    "url": "/static/media/hero-desktop.003b0c97.jpg"
+  }
+]);
